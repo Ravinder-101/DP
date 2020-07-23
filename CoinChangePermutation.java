3bd197b931aea0f4e7f8f20ff855a6ce40114bb8
Ravinder-101 <55032581+Ravinder-101@users.noreply.github.com>
2020-07-23 18:12:49 +0530

    Update CoinChangePermutation.java

--- CoinChangePermutation.java
+++ CoinChangePermutation.java
@@ -3,29 +3,25 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception { 
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int arr[] = new int[n];
-        for(int i=0;i<n;i++){
-            arr[i] = scn.nextInt();
+        int coins[] = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            coins[i] = scn.nextInt();
         }
         int tar = scn.nextInt();
-        
-        System.out.print(targetsum(arr, tar,0 , 0 ));
-    }
-    public static boolean  targetsum(int arr[] , int tar ,int i, int asf){
-        if(i== arr.length){
-            return false;
+        int dp []  = new int[tar+1];
+        dp[0] = 1;
+        for(int i=1;i<dp.length;i++){
+            for(int coin:coins){
+                if(i>=coin){
+                    dp[i] += dp[i-coin];
+                }
+            }
         }
-        if( tar < asf ){
-            return false;
-        }
-        if( tar == asf ){
-            return true;
-        } 
-        
-        boolean result = targetsum(arr,tar,i+1, asf + arr[i]) ||targetsum(arr, tar,i+1, asf ) ;
-        return result;
+        System.out.print(dp[tar]);
+
     }
 }
